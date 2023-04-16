0e6cb209d1339792b64486d29191e9a9d44090b7
sidneymarcelofranco <sidneymarcelofranco@hotmail.com>
2023-04-15 23:23:26 -0300

    Criada a URL de inscrever_Evento

--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Evento
@@ -41,7 +41,7 @@ def novo_evento(request):
         messages.add_message(request, constants.SUCCESS, 'Evento cadastrado com sucesso')
         return redirect(reverse('novo_evento'))
 
-
+@login_required
 def gerenciar_evento(request):
     if request.method == "GET":
         nome = request.GET.get('nome')
@@ -50,3 +50,10 @@ def gerenciar_evento(request):
             eventos = eventos.filter(nome__contains=nome)
 
         return render(request, 'gerenciar_evento.html', {'eventos': eventos})
+
+@login_required
+def inscrever_evento(request, id):
+		# Validar login
+    evento = get_object_or_404(Evento, id=id) # da Classe Evento busque o Id
+    if request.method == "GET":
+        return render(request, 'inscrever_evento.html', {'evento': evento})    
